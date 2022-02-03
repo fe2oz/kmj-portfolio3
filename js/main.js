b935626cd26633c380540004b93924c5b53ea510
fe2oz <93234748+fe2oz@users.noreply.github.com>
2022-02-03 14:34:45 +0900

    02.03/14:34

--- js/main.js
+++ js/main.js
@@ -30,61 +30,67 @@ $(document).ready(function(){ //////메뉴
     tooltipFillColor: "rgba(51, 51, 51, 0.55)",
     data: {
     labels: [
-      "1. html",
-      "2. css",
-      "3. scss",
-      "4. javascript"
+      "html",
+      "css",
+      "scss",
+      "javascript",
+      "photoshop",
+      "illustrator",
+      "3d blender"
     ],
     datasets: [{
-    data: [90, 90, 65, 60],
+    data: [90, 90, 65, 60, 40, 30, 45],
     backgroundColor: [
       "orange",
       "skyblue",
       "pink",
-      "yellow"
+      "yellow",
+      "#3838b2",
+      "#eb9345",
+      "#b07ce1"
     ],
     hoverBackgroundColor: [
       "#f97338",
       "#5381d6",
       "#ff3ab3",
-      "#ced37e"
+      "#ced37e",
+      "#3f3fc6",
+      "#ffc160",
+      "#bd76ff"
     ]
     }]
   },
     options: { responsive: false }
   }); 
 
-  var options = {
-    responsive: false
-  };
-  new Chart($("#canvas2"), {
-    type: 'doughnut',
-    tooltipFillColor: "rgba(51, 51, 51, 0.55)",
-    data: {
-    labels: [
-      "photoshop",
-      "illustrator",
-      "3d blender",
-      "javascript"
-    ],
-    datasets: [{
-    data: [90, 90, 75, 60],
-    backgroundColor: [
-      "orange",
-      "skyblue",
-      "pink",
-      "yellow"
-    ],
-    hoverBackgroundColor: [
-      "#f97338",
-      "#5381d6",
-      "#ff3ab3",
-      "#ced37e"
-    ]
-    }]
-  },
-    options: { responsive: false }
-  }); 
+  gsap.timeline({
+    scrollTrigger: {
+      trigger: ".grid-container",
+      start: "top top",
+      end: () => innerHeight * 4,
+      scrub: true,
+      pin: ".grid",
+      anticipatePin: 1
+    }
+  })
+  .set(".gridBlock:not(.centerBlock)", {autoAlpha: 0})
+  .to(".gridBlock:not(.centerBlock)", {duration: 0.1, autoAlpha: 1}, 0.001)
+  .from(".gridLayer", {
+    scale: 3.3333,
+    ease: "none",
+  });
+  
+  
+  // Images to make it look better, not related to the effect
+  const size = Math.max(innerWidth, innerHeight);
+  gsap.set('.gridBlock', {backgroundImage: i => `url(https://picsum.photos/${size}/${size}?random=${i})`});
+  
+  const bigImg = new Image;    
+  bigImg.addEventListener("load", function () {
+    gsap.to(".centerPiece .gridBlock", {autoAlpha: 1, duration: 0.5});
+  });
+  
+  bigImg.src = `https://picsum.photos/${size}/${size}?random=50`;  
 });
 
 $(function(){
