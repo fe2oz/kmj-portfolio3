6340c4dac0950702b1a3774ede7dcb74df0b6538
fe2oz <93234748+fe2oz@users.noreply.github.com>
2022-02-04 15:51:10 +0900

    02.04/15:51

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ $(document).ready(function(){ //////메뉴
 
         timeline.to("#overlay-min", 0.8, {
             top: "0%",
-            ease: "expo.inOut",
+            ease: "expo.InOut",
             opacity: "1"
         });
         timeline.staggerFrom("#overlay-min li", 0.4, {
@@ -16,9 +16,13 @@ $(document).ready(function(){ //////메뉴
 
         $(document).on("click", "#open-menu", function () {
             timeline.play();
+            $(".section").css({visibility: "hidden"},).animate({visibility: "hidden"}, 2000)
+            $("#slider").css({visibility: "hidden"}).animate({opacity:0}, 2000)
         });
         $(document).on("click", "a", function () {
             timeline.reverse();
+            $(".section").css({opacity:0, visibility: "visible"}).animate({opacity:1}, 2000)
+            $("#slider").css({opacity:0, visibility: "visible"}).animate({opacity:1}, 2000)
         });
 
 
@@ -39,7 +43,7 @@ $(document).ready(function(){ //////메뉴
       "3d blender"
     ],
     datasets: [{
-    data: [90, 90, 65, 60, 40, 30, 45],
+    data: [90, 90, 65, 50, 40, 30, 45],
     backgroundColor: [
       "orange",
       "skyblue",
@@ -62,35 +66,7 @@ $(document).ready(function(){ //////메뉴
   },
     options: { responsive: false }
   }); 
-
-  gsap.timeline({
-    scrollTrigger: {
-      trigger: ".grid-container",
-      start: "top top",
-      end: () => innerHeight * 4,
-      scrub: true,
-      pin: ".grid",
-      anticipatePin: 1
-    }
-  })
-  .set(".gridBlock:not(.centerBlock)", {autoAlpha: 0})
-  .to(".gridBlock:not(.centerBlock)", {duration: 0.1, autoAlpha: 1}, 0.001)
-  .from(".gridLayer", {
-    scale: 3.3333,
-    ease: "none",
-  });
-  
-  
-  // Images to make it look better, not related to the effect
-  const size = Math.max(innerWidth, innerHeight);
-  gsap.set('.gridBlock', {backgroundImage: i => `url(https://picsum.photos/${size}/${size}?random=${i})`});
-  
-  const bigImg = new Image;    
-  bigImg.addEventListener("load", function () {
-    gsap.to(".centerPiece .gridBlock", {autoAlpha: 1, duration: 0.5});
-  });
-  
-  bigImg.src = `https://picsum.photos/${size}/${size}?random=50`;  
+   
 });
 
 $(function(){
@@ -98,17 +74,26 @@ $(function(){
         $(".section-one-about-2-profile").css("display", "flex");
         $(".section-one-about-2-history").css("display", "none");
         $(".section-one-about-2-license").css("display", "none");
+        $("#deco1").stop().animate({"top": "55%"});
+        $("#deco2").stop().animate({"top": "25%"});
+        $("#deco3").stop().animate({"top": "10%"});
     });
 
     $("#history").mouseover(function(){
         $(".section-one-about-2-history").css("display", "flex");
         $(".section-one-about-2-profile").css("display", "none");
         $(".section-one-about-2-license").css("display", "none");
+        $("#deco1").stop().animate({"top": "30%"});
+        $("#deco2").stop().animate({"top": "45%"});
+        $("#deco3").stop().animate({"top": "40%"});
     });
 
     $("#license").mouseover(function(){
         $(".section-one-about-2-license").css("display", "flex");
         $(".section-one-about-2-profile").css("display", "none");
         $(".section-one-about-2-history").css("display", "none");
+        $("#deco1").stop().animate({"top": "40%"});
+        $("#deco2").stop().animate({"top": "30%"});
+        $("#deco3").stop().animate({"top": "25%"});
     });
 });
